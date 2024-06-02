461490d7c2d2af354c229e14c686e122e58d6e49
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-06-02 09:08:30 +0100

    added formatting

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ Portfolio exercise from frontend mentor.
 This project was built using TypeScript, Vite, and React.js
 
 Reference links:
-* Frontend exercise page: https://www.frontendmentor.io/solutions/singlepage-portfolio-page-ckpqOAYFBF
-* Live site: https://ghoulkingr.github.io/portfolio-exercise/
+
+-   Frontend exercise page: https://www.frontendmentor.io/solutions/singlepage-portfolio-page-ckpqOAYFBF
+-   Live site: https://ghoulkingr.github.io/portfolio-exercise/
 
 # Running this project locally
 
 To run this application on your machine run these lines in your terminal:
+
 ```bash
 # clone this project
 git clone https://github.com/GhoulKingR/portfolio-exercise.git
--- src/components/NameLinksHead.tsx
+++ src/components/NameLinksHead.tsx
@@ -1,27 +1,26 @@
-import styled from "styled-components";
-import NameLinks from "./NameLinks";
-
+import styled from 'styled-components';
+import NameLinks from './NameLinks';
 
 function NameLinksHead() {
-  return (
-    <Section>
-      <NameLinksCover>
-        <NameLinks />
-      </NameLinksCover>
-    </Section>
-  )
+    return (
+        <Section>
+            <NameLinksCover>
+                <NameLinks />
+            </NameLinksCover>
+        </Section>
+    );
 }
 
 export default NameLinksHead;
 
 const NameLinksCover = styled.div`
-  width: 100%;
-  height: 0px;
-  padding-top: 20px;
+    width: 100%;
+    height: 0px;
+    padding-top: 20px;
 `;
 
 const Section = styled.header`
-  text-align: center;
-  background-color: transparent;
-  margin-top: -20px;
+    text-align: center;
+    background-color: transparent;
+    margin-top: -20px;
 `;
